f40a1459c5eb621881115fd86c3039d6be77eb0b
Tharuna07 <tharunasrilakshmi7@gmail.com>
2024-07-10 16:01:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+app.use("/users", userRoutes);
+app.use("/products", cartRoutes);
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://tharuna07:Tharuna%407@cluster0.utjdpwt.mongodb.net/myCrudApp?retryWrites=true&w=majority"
+    );
+    console.log("database is connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.listen(4000, () => {
+  dbConnect();
+  console.log("server started on port 4000");
+});
